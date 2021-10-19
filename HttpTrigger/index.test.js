4a2e965b9902d14da2f6907c96febb6d2dd98d86
fernando_cohuo98@hotmail.com <fernando_cohuo98@hotmail.com>
2021-10-19 00:22:32 -0500

    primer commit

---
+++ HttpTrigger/index.test.js
@@ -0,0 +1,12 @@
+const httpFunctions = require('./index');
+const context = require('../testing/Context');
+
+test('Http trigger example', async () => {
+  const request = {
+    query: { name: 'schuc' },
+  };
+  await httpFunctions(context, request);
+  expect(context.res.body).toContain('axample');
+  expect(context.res.body).toEqual('Welcome, schuc example');
+  expect(context.log.mock.calls.length).toBe(1);
+});
