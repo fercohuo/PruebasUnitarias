427ab9804885b6b33d08bc03219685819832b619
fercohuo <58056471+fercohuo@users.noreply.github.com>
2021-10-19 00:40:18 -0500

    Update index.test.js

--- HttpTrigger/index.test.js
+++ HttpTrigger/index.test.js
@@ -3,10 +3,10 @@ const context = require('../testing/Context');
 
 test('Http trigger example', async () => {
   const request = {
-    query: { name: 'schuc' },
+    query: { name: 'coh' },
   };
   await httpFunctions(context, request);
   expect(context.res.body).toContain('axample');
-  expect(context.res.body).toEqual('Welcome, scoh example');
+  expect(context.res.body).toEqual('Welcome, coh example');
   expect(context.log.mock.calls.length).toBe(1);
 });
